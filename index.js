9f589116633d4ca287b5dac2d0263397199b89c9
Andy Moeller <ar.moeller1@gmail.com>
2019-01-30 01:18:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const moveDodgerLeft = () => {
   const dodger = document.getElementById('dodger')
   //change the layout/position of dodger - need some css changes
   const currentLeft = dodger.style.left //'180px'
-  const currentLeftNumber = currentLeft.replace('px', '')//180
-  currentLeftNumber = parseInt(currentLeftNumber, 10)
+  const currentLeftNumber = currentLeft.replace('px', '')//'180'
+  currentLeftNumber = parseInt(currentLeftNumber, 10)//180
+  dod.style.left = currentLeftNumber - 1 + 'px' //'179px'
 }
 
 const moveDodgerRight = () => {
