c22a863ff4261b5555c419c623bff655b57e5e8d
Andy Moeller <ar.moeller1@gmail.com>
2019-01-30 00:52:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 //Event Listener
 // 1 FInd the thing to do the Event on 
+// we actually dont need an element (can skip 1, document will get event listener)
+//2 write the Function that will happen when we do the thing
 
+const moveDodgerLeft = () => {
+  
+}
